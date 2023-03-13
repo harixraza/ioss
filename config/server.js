217da126e30091653325d28bed372835d374ae32
harixraza <106744750+harixraza@users.noreply.github.com>
2023-03-13 11:31:19 +0500

    app keys added

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: 'vAGlZMrjKoYS+BCI8jAA3g==,yyphqbB+XJw299a7M1oKWQ==,vq/8MvG+4yiX1M/hD0NYqg==,m5DMIcsXd/qAKRJIOEW8Vg==',
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
